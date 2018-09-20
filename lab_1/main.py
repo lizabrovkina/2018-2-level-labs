9cd7d0e5e5bc2fe1b31d18c9b3a6d8ebc0f51935
lizabrovkina <43420806+lizabrovkina@users.noreply.github.com>
2018-09-20 12:53:07 +0300

    Update main.py

--- lab_1/main.py
+++ lab_1/main.py
@@ -9,16 +9,16 @@ def calculate_frequences() -> dict:
     """
     Calculates number of times each word appears in the text
     """
-    pass
+    a = {}
 
 def filter_stop_words() -> dict:
     """
     Removes all stop words from the given frequencies dictionary
     """
-    pass
+    a = {}
 
 def get_top_n() -> tuple:
     """
     Takes first N popular words
     """
-    pass
+    a = {}
